dbf6639e128a7bcae4b8998c9cc074697b49e0ad
SPWizard01 <the.mtrix@gmail.com>
2024-10-22 22:13:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Bun Dynamic Path Plugin
+
+## Features
+
+Adds ability to dynamically resolve paths to assets that are loaded with `file` loader.
+
+This is useful when you are not in control of paths or the environment and just want to insert your code.
+
+It uses `import.meta.resolve` to resolve path relative to current ESM module.
+
+```typescript
+//build.ts
+import { build } from "bun";
+await build({
+  entrypoints: ["./index.ts"],
+  naming: {
+    asset: "assets/[name].[ext]",
+  },
+  outdir: "dist",
+  plugins: [dynamicPathPlugin({ fileExtensions: ["jpg"] })],
+});
+```
+
+Resulting output will be something like:
+
+```javascript
+// E:/Path/To/Your/Asset/assets/blueprint.jpg
+var blueprint_default = "./assets/blueprint.jpg";
+var init_blueprint = () => {};
+
+// E:/Path/To/Your/Asset/assets/blueprint.jpg
+var resolvePath, blueprint_default2;
+var init_blueprint2 = __esm(() => {
+  init_blueprint();
+  resolvePath = import.meta.resolve(blueprint_default);
+  blueprint_default2 = resolvePath;
+});
+```
+
+## Options
+
+`fileExtensions`: Should be same file types that you configured bun to copy over to the output directory. Example: `["jpg","woff"]`
+`includeHash`: Optional, if provided adds `?[file_hash]` to the output.
+Example:
+```javascript
+// E:/Path/To/Your/Asset/assets/blueprint.jpg
+var blueprint_default = "./assets/blueprint.jpg";
+var init_blueprint = () => {
+};
+
+// hash:E:/Path/To/Your/Asset/assets/blueprint.jpg
+var resolvePath, blueprint_default2;
+var init_blueprint2 = __esm(() => {
+  init_blueprint();
+  resolvePath = import.meta.resolve(`${blueprint_default}?e9577a8b3184f0f8bdfe5202b7060bfd6fbe8c76`);
+  blueprint_default2 = resolvePath;
+});
+
+```
---
+++ index.ts
@@ -0,0 +1,73 @@
+import { type BunPlugin, CryptoHasher } from "bun"
+import { resolve, dirname } from "path";
+import { type } from "os";
+export interface DynamicPathPluginOptions {
+    /**
+     * File extensions to be considered for dynamic path resolution
+     * 
+     * Example: `[ 'jpg', 'png', 'woff' ]`
+     * 
+     * Should be same file types that you configured bun to copy over to the output directory
+     */
+    fileExtensions: string[]
+    includeHash?: boolean
+}
+const pluginName = `bun-dynamic-path`
+const hasher = new Bun.CryptoHasher("sha1");
+
+async function getContentHash(filePath: string) {
+    const fileBytes = await Bun.file(filePath).bytes();
+    hasher.update(fileBytes);
+    const result = hasher.digest("hex");
+    return result;
+}
+
+export default function dynamicPathPlugin(pluginConfig: DynamicPathPluginOptions): BunPlugin {
+    return {
+        name: pluginName,
+        target: "browser",
+        setup: ({ onLoad, onResolve, module, config }) => {
+            const filterRegex = new RegExp(`\\.(${pluginConfig.fileExtensions.join('|')})$`)
+            onResolve({ filter: filterRegex, namespace: "file" }, async (args) => {
+                if (args.path.startsWith(pluginName)) {
+                    return {
+                        path: args.path.replace(`${pluginName}:`, ''),
+                        //namespace: "file"
+                    }
+                }
+                const relativePath = args.path
+                const fromPath = args.importer
+                const rs = resolve(dirname(fromPath), relativePath)
+                let pretty = type() === 'Windows_NT' ? rs.replace(/\\/g, '/') : rs
+                if(pluginConfig.includeHash){
+                    pretty = `hash:${pretty}`
+                }
+
+                return {
+                    path: pretty,
+                    namespace: pluginName,
+                }
+
+            })
+            onLoad({ filter: /./, namespace: pluginName }, async (args) => {
+                let importString = `assetPath`
+                let path = args.path
+                if (path.startsWith("hash:")) {
+                    path = path.replace("hash:", "")
+                    const hash = await getContentHash(path)
+                    importString = `\`\${assetPath}?${hash}\``
+                }
+                return {
+                    contents: `
+                        import assetPath from '${pluginName}:${path}';
+                        const resolvePath = import.meta.resolve(${importString});
+                        export default resolvePath;
+                    `,
+                    loader: "js",
+
+
+                }
+            })
+        }
+    }
+}
